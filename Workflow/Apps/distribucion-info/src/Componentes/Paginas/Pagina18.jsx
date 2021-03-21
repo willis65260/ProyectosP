57ab3e62d928688fc1a61f19e2fcd9b62b19fc0e
willis65260 <48706135+willis65260@users.noreply.github.com>
2021-03-21 04:14:40 -0600

    FIN

--- Workflow/Apps/distribucion-info/src/Componentes/Paginas/Pagina18.jsx
+++ Workflow/Apps/distribucion-info/src/Componentes/Paginas/Pagina18.jsx
@@ -3,32 +3,6 @@ import React from 'react';
 
 import '../../CSS/Bootstrap/css/bootstrapD.css'
 
-import InofrmacionVaria from '../Cuerpo/Informacion-varia';
-
-
-import Logo from '../Banner/Logo';
-import Contacto from '../Cuerpo/Contacto';
-
-
-import Buscar from '../header/buscar';
-
-
-import DerechosDeAutor from '../Footer/DerechosDeAutor';
-
-import LinksVarios from '../Cuerpo/LinksVarios';
-
-
-
-import Menu from '../Cuerpo/Menu';
-import VideoPresentacion from '../Cuerpo/VideoPresentacion';
-import Informacion from '../header/informacion';
-
-import Ubicacion from '../Cuerpo/Ubicacion';
-import RedesSociales from '../Footer/RedesSociales';
-import Covid from '../header/covid-info';
-import Libreria from '../Cuerpo/Libreria';
-import Noticias from '../header/Noticias';
-import Staff from '../Cuerpo/Staff';
 import Banner from '../Banner/Banner';
 import Cuerpo from '../Cuerpo/Cuerpo';
 
