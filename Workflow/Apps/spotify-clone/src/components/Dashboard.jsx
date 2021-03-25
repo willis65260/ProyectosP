a9e054a6bd7a8203525d22767b3ce251fc0fc1d2
willis65260 <48706135+willis65260@users.noreply.github.com>
2021-03-25 00:45:01 -0600

    spotify clon

---
+++ Workflow/Apps/spotify-clone/src/components/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import useAuth from '../hooks/useAuth'
+
+export default function Dashboard({code}) {
+    const accessToken = useAuth(code)
+    return (
+        <div>{code}</div>
+    )
+}
